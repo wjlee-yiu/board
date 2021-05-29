af0e126a53355f8e2742b1ee3289bbfedbf1c26d
Wanjoo <wjlee@yiu.ac.kr>
2021-05-30 04:02:27 +0900

    mysql table script

---
+++ board.sql
@@ -0,0 +1,19 @@
+create database board;
+
+create table member (
+  id varchar(20) not null primary key,
+  pw varchar(20) not null,
+  name varchar(40) not null,
+  email varchar(40) not null
+)
+
+create table bbs (
+  bbsid int auto_increment primary key,
+  bbstitle varchar(80) not null,
+  bbscontent varchar(1000),
+  bbsdate datetime default CURRENT_TIMESTAMP,
+  bbshit int default 0,
+  bbscategoty varchar(40),
+  id varchar(20)
+  );
+)
